e416881fabf59f958ef134304b9216c87f41d700
Sowrov <mdsowrov315@gmail.com>
2021-10-04 09:57:56 +0300

    err fixed

--- src/components/Homecard/Homecard.js
+++ src/components/Homecard/Homecard.js
@@ -12,8 +12,7 @@ return (
         <Card.Img variant="top" src={img} />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
-          <Card.Title>{title}</Card.Title>
-          <Card.Text>Rating: {rating}</Card.Text>
+          <Card.Text>Rating: {rating} </Card.Text>
           <Card.Text>Student: {student}</Card.Text>
           <Card.Text>{Cirtificate}</Card.Text>
         </Card.Body>
